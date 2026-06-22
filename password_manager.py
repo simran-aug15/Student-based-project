74290635f2a10c348b649dc529aefe4e2cdba0ab
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2026-06-22 15:10:46 +0530

    Auto commit

--- password_manager.py
+++ password_manager.py
@@ -1,2 +1,12 @@
 import random
 import string
+
+
+passwords ={}
+
+#load exciting password file
+try:
+    with open("passwords.txt",'r')as file:
+        for line in file:
+            website, pwd=line.strip().split(":")
+            passwords[website] =pwd

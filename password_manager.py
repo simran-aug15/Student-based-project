071453a29a8517b8686a92bdd456436136b69da3
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2026-06-22 15:16:45 +0530

    Auto commit

--- password_manager.py
+++ password_manager.py
@@ -18,3 +18,11 @@ def generatoe_password():
     chars=string.ascii_letters + string.digits +"!@#$%&"
     password="".join(random.choice(chars) for _ in range(8))
     return password
+
+
+while True:
+    print("\n---- PERSONAL PASSWORD MANAGER---")
+    print("1. Save password")
+    print("2. View password")
+    print("3. Generate password")
+    print("Exit")
